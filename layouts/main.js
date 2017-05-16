cb3c18c88467056a4d2c5bdf9e20bdab459866c8
Jesús Lobos <jlobos@outlook.cl>
2017-05-16 00:57:35 -0400

    add Google Analytics

--- layouts/main.js
+++ layouts/main.js
@@ -1,15 +1,20 @@
 import Head from 'next/head'
 
-// <link href='https://cdn.rawgit.com/tonsky/FiraCode/master/distr/ttf/FiraCode-Regular.ttf' rel='stylesheet' />
-// font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
-
 export default ({ children }) => (
   <div>
     <Head>
       <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta name='theme-color' content='#000000' />
 
-      <link href='https://fonts.googleapis.com/css?family=Fira+Mono|Vibur' rel='stylesheet' />
+      <link href='https://fonts.googleapis.com/css?family=Vibur' rel='stylesheet' />
       <link href='https://cdn.rawgit.com/tonsky/FiraCode/master/distr/ttf/FiraCode-Regular.ttf' rel='stylesheet' />
+
+      {/* Google Analytics */}
+      <script dangerouslySetInnerHTML={{ __html: `
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-93825702-2', 'auto');
+        ga('send', 'pageview');
+        `}} />
     </Head>
 
     { children }
