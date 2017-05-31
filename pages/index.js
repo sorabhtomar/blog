4d4f89ba842c2b9816ccc7293ed098b7fdf3333f
Jesús Lobos <jlobos@outlook.cl>
2017-05-31 17:42:44 -0400

    add projects

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head'
-import { posts } from '../posts'
+import { posts, projects } from '../db'
 import Page from '../layouts/main'
 import withViews from '../lib/with-views'
 import Triangle from '../components/triangles/main'
 import Footer from '../components/footer'
 import Post from '../components/post'
+import Project, { Title } from '../components/project'
 
 export default withViews(({ views }) => (
   <Page>
@@ -19,6 +20,13 @@ export default withViews(({ views }) => (
         {
           posts.map(({ id, date, title }) => (
             <Post id={id} key={id} date={date} title={title} />
+          ))
+        }
+
+        <Title />
+        {
+          projects.map(({ id, title }) => (
+            <Project id={id} key={id} title={title} />
           ))
         }
       </div>
