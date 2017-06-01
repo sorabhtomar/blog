ffeb1ee665e8d02a57cd41ce310e9642591d5fc3
Jesús Lobos <jlobos@outlook.cl>
2017-06-01 15:04:11 -0400

    add compresion

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Packages
 const express = require('express')
 const next = require('next')
+const compression = require('compression')
 
 const {
   NODE_ENV,
@@ -14,6 +15,7 @@ const handle = app.getRequestHandler()
 app.prepare()
 .then(() => {
   const server = express()
+  server.use(compression())
 
   server.get('*', (req, res) => handle(req, res))
 
