9c11badca27143d435fed87a0fef1c3ee17ea2fc
Jesús Lobos <jlobos@outlook.cl>
2017-07-24 23:12:11 -0400

    remove prefetch of link

--- src/components/post.js
+++ src/components/post.js
@@ -1,6 +1,5 @@
 // Packages
 import React from 'react'
-import Link from 'next/link'
 import moment from 'moment'
 
 const Header = ({ title }) =>
@@ -54,23 +53,16 @@ export const Footer = ({ createdAt, readingTime }) =>
     `}</style>
   </footer>
 
-// <Link prefetch href={`/${props.slug}`}>
 export default props =>
   <article>
-    <Link
-      prefetch
-      href={{ pathname: '/post', query: { slug: props.slug } }}
-      as={`/${props.slug}`}
-    >
-      <a href={`/${props.slug}`}>
-        <Header title={props.title} />
-        <Section description={props.virtuals.subtitle} />
-        <Footer
-          createdAt={props.createdAt}
-          readingTime={props.virtuals.readingTime}
-        />
-      </a>
-    </Link>
+    <a href={`/${props.slug}`}>
+      <Header title={props.title} />
+      <Section description={props.virtuals.subtitle} />
+      <Footer
+        createdAt={props.createdAt}
+        readingTime={props.virtuals.readingTime}
+      />
+    </a>
 
     <style jsx>{`
       article {
