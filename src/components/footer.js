b4d2389a0c281d7f9477c7afdadc85d235add76c
Jesús Lobos <jlobos@outlook.cl>
2017-10-03 00:00:30 -0300

    update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,19 +1,33 @@
-export default ({ children }) =>
+export default ({ children }) => (
   <footer>
     {children}
 
     <nav>
-      <a href='mailto:jlobitu@gmail.com'>© 2017 — jlobos</a>
-      <a href='/politica-de-privacidad'>Política de Privacidad</a>
+      <div>
+        <a href='/'>© 2017 jlobos</a>
+      </div>
+      <div>
+        <p>
+          El código fuente del sitio web está disponible en{' '}
+          <a href='https://github.com/jlobos/blog'>GitHub</a>. Todo el código y
+          el contenido de este sitio se publican bajo la licencia MIT.
+        </p>
+      </div>
+      <div>
+        <a href='/politica-de-privacidad'>Política de Privacidad</a>
+      </div>
     </nav>
 
     <style jsx>{`
       nav {
-        display: flex;
-        justify-content: space-between;
         margin: 0 auto;
         max-width: 768px;
         padding: 2rem 1rem;
+        text-align: center;
+      }
+
+      div {
+        padding-bottom: 1rem;
       }
 
       a {
@@ -26,3 +40,4 @@ export default ({ children }) =>
       }
     `}</style>
   </footer>
+)
